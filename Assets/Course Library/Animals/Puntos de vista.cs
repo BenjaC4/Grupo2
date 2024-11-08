9eae0cac748eba77760fbe493214780d080f257b
BenjaC4 <benjamin.castillo@alumnos.upla.cl>
2024-11-08 14:50:00 -0300

    Agregacion de logica de camaras y edicion a la escena de prueba
    se agrego la funcion de cambio de camaras y ademas se ordeno de mejor manera los elementos de la escena de prueba para que quede mas ordenado.

---
+++ Assets/Course Library/Animals/Puntos de vista.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Cinemachine;
+
+public class Puntodevista : MonoBehaviour
+{
+    private Canvas hud;
+    public CinemachineCamera camIsometric;
+    public CinemachineCamera camTopView;
+    public CinemachineCamera camIsometricZoom;
+
+    private CinemachineCamera[] cameras;
+    private int CamaraActual = 0;
+    public float hudDistance = 15f;
+
+    void Start()
+    {
+        //Encuentra el hud para poder utilizarlo
+        hud = FindAnyObjectByType<Canvas>();
+        //En la lista tipo CinemachineCamera creo 3 camaras tipo Cinemachinecamera
+        //Estas camaras deben ser seleccionadadas desde el editor de Unity
+        cameras = new CinemachineCamera[] { camIsometric, camTopView, camIsometricZoom };
+        
+        SetActiveCamera(0);
+        UpdateHUDCamera(); 
+    }
+
+    void Update()
+    {
+        //Espera el input de la tecla v para cambiar las camaras
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            //Recorre la lista de cameras
+            CamaraActual = (CamaraActual + 1) % cameras.Length;
+            SetActiveCamera(CamaraActual);
+            UpdateHUDCamera(); 
+        }
+    }
+
+    void SetActiveCamera(int n)
+    {
+        //Cambia las prioridades de las camaras y asi pone la seleccionada a la vista
+        foreach (var cam in cameras)
+        {
+            cam.Priority = 0;
+        }
+        cameras[n].Priority = 1;
+    }
+
+    void UpdateHUDCamera()
+    {
+
+        if (hud != null)
+        {
+            //Con estas lineas de codigo hacemos que el hud se fije en la camara actual
+            Camera mainCamera = Camera.main;
+            hud.worldCamera = mainCamera;
+            hud.planeDistance = hudDistance;
+        }
+    }
+}
